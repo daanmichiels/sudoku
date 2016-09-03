f1c6d3d1f43ca9efd70e4d4c7d6dd780971245f5
Daan Michiels <michielsdaan@gmail.com>
2016-09-02 23:35:39 -0500

    Highlight selected cell

--- sudoku.js
+++ sudoku.js
@@ -1,4 +1,5 @@
 
+
 var Sudoku = function(m, n) {
 	this.m = m;
 	this.n = n;
@@ -14,7 +15,26 @@ var Sudoku = function(m, n) {
 	}
 }
 
-Sudoku.prototype.toTable = function() {
+Sudoku.prototype.show = function() {
+	for(var i = 0; i < this.m * this.n; ++i) {
+		for(var j = 0; j < this.m * this.n; ++j) {
+			var cell = document.getElementById(i + ";" + j);
+			cell.innerHTML = this.grid[i][j] == -1 ? "&nbsp;" : this.grid[i][j];
+		}
+	}
+}
+
+var onMouseEnter = function(i, j) {
+	var cell = document.getElementById(i + ";" + j);
+	cell.classList.add("hover-strong");
+}
+
+var onMouseLeave = function(i, j) {
+	var cell = document.getElementById(i + ";" + j);
+	cell.classList.remove("hover-strong");
+}
+
+var createHTML = function(m, n) {
 	var table = "<table>";
 	for(var i = 0; i < this.m * this.n; ++i) {
 		table += "<tr>";
@@ -27,30 +47,29 @@ Sudoku.prototype.toTable = function() {
 				classes.push("thickleft");
 			}
 			table += "<td id=\"";
-			table += i+";"+j;
+			table += i + ";" + j;
 			table += "\"";
 			if(classes.length > 0) {
 				table += " class=\"";
 				table += classes.join(" ");
 				table += "\"";
 			}
-			table += ">";
-			if(this.grid[i][j] == -1) {
-				table += "&nbsp;";
-			} else {
-				table += this.grid[i][j];
-			}
-			table += "</td>";
+			table += " onmouseenter=\"onMouseEnter(" + i + ", " + j + ");\""
+			table += " onmouseleave=\"onMouseLeave(" + i + ", " + j + ");\"></td>";
 		}
 		table += "</tr>";
 	}
 	table += "</table>";
-	return table;
+
+	div = document.getElementById("sudoku");
+	div.innerHTML = table;
 }
 
 window.onload = function() {
-	s = new Sudoku(3,3);
+	m = 3;
+	n = 2;
+	createHTML(m, n);
+	s = new Sudoku(m, n);
 	s.grid[1][2] = 3;
-	div = document.getElementById("sudoku");
-	div.innerHTML = s.toTable();
+	s.show();
 }
