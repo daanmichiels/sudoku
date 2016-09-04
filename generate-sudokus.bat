dbb48a4ef4c289a9eb9bc57ca6fa36928d943db4
Daan Michiels <michielsdaan@gmail.com>
2016-09-04 12:01:34 -0500

    Improve sudoku generation script

--- generate-sudokus.bat
+++ generate-sudokus.bat
@@ -1,4 +1,4 @@
-java -jar qqwing-1.3.4.jar --generate 50 --difficulty simple --one-line --csv --stats > easy.csv
-java -jar qqwing-1.3.4.jar --generate 50 --difficulty easy --one-line --csv --stats > medium.csv
-java -jar qqwing-1.3.4.jar --generate 50 --difficulty intermediate --one-line --csv --stats > hard.csv
-java -jar qqwing-1.3.4.jar --generate 50 --difficulty expert --one-line --csv --stats > very-hard.csv
+java -jar qqwing-1.3.4.jar --generate 50 --difficulty simple --one-line --csv > level1.csv
+java -jar qqwing-1.3.4.jar --generate 50 --difficulty easy --one-line --csv > level2.csv
+java -jar qqwing-1.3.4.jar --generate 50 --difficulty intermediate --one-line --csv > level3.csv
+java -jar qqwing-1.3.4.jar --generate 50 --difficulty expert --one-line --csv > level4.csv
