5a13fa13de1ad38346b7f64ea68cb8fe16671622
Daan Michiels <michielsdaan@gmail.com>
2016-09-04 12:12:55 -0500

    Improve puzzle generation script

--- generate-sudokus.bat
+++ generate-sudokus.bat
@@ -4,7 +4,7 @@
 :: This script generates sudokus of the first 4 levels.
 :: The puzzles of level 5 are hand-picked.
 
-java -jar qqwing-1.3.4.jar --generate 50 --difficulty simple --one-line --csv > level1.csv
-java -jar qqwing-1.3.4.jar --generate 50 --difficulty easy --one-line --csv > level2.csv
-java -jar qqwing-1.3.4.jar --generate 50 --difficulty intermediate --one-line --csv > level3.csv
-java -jar qqwing-1.3.4.jar --generate 50 --difficulty expert --one-line --csv > level4.csv
+java -jar qqwing-1.3.4.jar --generate 50 --difficulty simple --one-line > level1.txt
+java -jar qqwing-1.3.4.jar --generate 50 --difficulty easy --one-line > level2.txt
+java -jar qqwing-1.3.4.jar --generate 50 --difficulty intermediate --one-line > level3.txt
+java -jar qqwing-1.3.4.jar --generate 50 --difficulty expert --one-line > level4.txt
