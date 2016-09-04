dadc4c350b09709a50e18f22da5ed6f15a4e934c
Daan Michiels <michielsdaan@gmail.com>
2016-09-04 12:03:55 -0500

    Clarify sudoku generation script

--- generate-sudokus.bat
+++ generate-sudokus.bat
@@ -1,3 +1,9 @@
+:: Please place the QQWing java version (e.g. qqwing-1.3.4.jar) in the working directory
+:: before running this script.
+
+:: This script generates sudokus of the first 4 levels.
+:: The puzzles of level 5 are hand-picked.
+
 java -jar qqwing-1.3.4.jar --generate 50 --difficulty simple --one-line --csv > level1.csv
 java -jar qqwing-1.3.4.jar --generate 50 --difficulty easy --one-line --csv > level2.csv
 java -jar qqwing-1.3.4.jar --generate 50 --difficulty intermediate --one-line --csv > level3.csv
