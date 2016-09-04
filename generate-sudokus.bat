48fae7418a7f698609db2ada7a7e8eea73391ed6
Daan Michiels <michielsdaan@gmail.com>
2016-09-04 11:59:42 -0500

    Add sudoku generation script
    uses Java version of QQWing

---
+++ generate-sudokus.bat
@@ -0,0 +1,4 @@
+java -jar qqwing-1.3.4.jar --generate 50 --difficulty simple --one-line --csv --stats > easy.csv
+java -jar qqwing-1.3.4.jar --generate 50 --difficulty easy --one-line --csv --stats > medium.csv
+java -jar qqwing-1.3.4.jar --generate 50 --difficulty intermediate --one-line --csv --stats > hard.csv
+java -jar qqwing-1.3.4.jar --generate 50 --difficulty expert --one-line --csv --stats > very-hard.csv
